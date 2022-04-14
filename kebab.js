5c73520656f309c3dc5cbca57b12912d073d79e2
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-04-14 13:25:46 +0200

    Add jest conf

---
+++ kebab.js
@@ -0,0 +1,13 @@
+const isKebabCase = (textToCheck) => {
+  const x = "";
+};
+
+const formatKebabToLowerCamelCase = (textToCheck) => {};
+
+const showMessageToUser = () => {};
+
+isKebabCase(textToCheck)
+  ? formatKebabToLowerCamelCase(textToCheck)
+  : showMessageToUser();
+
+isKebabCase("ohMama");
